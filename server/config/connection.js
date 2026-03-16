9797a682c2577476f73902abe1673af4e82952e9
mojsun <m.khorashahi7@gmail.com>
2026-03-15 21:46:43 -0400

    Remove unsupported Mongo option autoselectfamily

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,14 +3,19 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/getaway';
 
 // Base options (supported by all MongoDB driver versions)
+// const options = {
+//   serverSelectionTimeoutMS: 10000,
+// };
+
+// Only add autoSelectFamily for Atlas (Heroku). Older drivers don't support it locally.
+// if (uri.includes('mongodb.net')) {
+//   options.autoSelectFamily = false;
+// }
+
+// Simple, supported options only
 const options = {
   serverSelectionTimeoutMS: 10000,
 };
-
-// Only add autoSelectFamily for Atlas (Heroku). Older drivers don't support it locally.
-if (uri.includes('mongodb.net')) {
-  options.autoSelectFamily = false;
-}
 
 mongoose.connect(uri, options);
 
